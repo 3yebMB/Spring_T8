9d4b9477756c9a1f2a30b45ef15ec3b7cae16505
3yebMB <3yebmb@gmail.com>
2019-02-25 13:48:40 +0500

    added spring project

--- src/main/java/io/spring/test/SpringBootWebApplication.java
+++ src/main/java/io/spring/test/SpringBootWebApplication.java
@@ -1,4 +1,4 @@
-package ru.artem;
+package io.spring.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/io/spring/test/controllers/WelcomeController.java
+++ src/main/java/io/spring/test/controllers/WelcomeController.java
@@ -1,4 +1,4 @@
-package ru.artem.controllers;
+package io.spring.test.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
