d49cc9e00762c33dd0093983a8c431c1f8119eae
3yebMB <3yebmb@gmail.com>
2019-02-27 16:00:22 +0500

    updated controller

--- src/main/java/io/spring/test/controllers/WelcomeController.java
+++ src/main/java/io/spring/test/controllers/WelcomeController.java
@@ -11,13 +11,13 @@ public class WelcomeController {
 
     @RequestMapping("/")
     public String welcome(Map<String, Object> model) {
-        model.put("message", "Java!");
+        model.put("message", "Android");
         return "welcome";
     }
 
     @RequestMapping("/info")
     public String info(Map<String, Object> model) {
-//        model.put("message", "Java!");
+        model.put("message", "The Chelyabinsk meteor entered Earth's atmosphere over Russia on 15 February 2013 at about 09:20 YEKT (03:20 UTC)");
         return "info";
     }
 
@@ -25,6 +25,12 @@ public class WelcomeController {
     public String about(Map<String, Object> model) {
         model.put("test_param", param);
         return "about";
+    }
+
+    @RequestMapping("/blank")
+    public String blank(Map<String, Object> model) {
+        model.put("test_param", param);
+        return "blank";
     }
 
     @RequestMapping(value = "/printme/{data}", method = RequestMethod.GET)
@@ -36,6 +42,13 @@ public class WelcomeController {
     @RequestMapping(value = "/about/test_param", method = RequestMethod.POST)
     @ResponseBody
     public void addNewCustomer(@RequestParam("name_param") String nameParam) {
+        param = nameParam;
+        System.out.println(nameParam);
+    }
+
+    @RequestMapping(value = "/blank/test_param", method = RequestMethod.POST)
+    @ResponseBody
+    public void addNewBlank(@RequestParam("name_param") String nameParam) {
         param = nameParam;
         System.out.println(nameParam);
     }
